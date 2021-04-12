07c6bc8c40287822feccf1e1db39d8e49b7172bf
Will Rouesnel <wrouesnel@wrouesnel.com>
2021-04-12 14:48:57 +1000

    More Win 10 fixes.

--- win-common/extra/scripts/firewall-misc.ps1
+++ win-common/extra/scripts/firewall-misc.ps1
@@ -6,3 +6,19 @@ New-NetFirewallRule `
     -Protocol ICMPv4 `
     -IcmpType 8 `
     -Action Allow
+
+# Note: in theory this should be done in the Unattend.xml file. In practice,
+# once Win 10 gets involved, it seems almost impossible to make that file work
+# reliably. So we do our firewall provisioning for Remote Desktop here.
+
+# Disable Network Discovery
+Write-Verbose "Disabling Network Discovery Rules"
+foreach ($rule in Get-NetFirewallRule -DisplayGroup "Network Discovery") {
+    Set-NetFirewallRule -Name $rule.Name -Enabled False
+}
+
+# Enable Remote Desktop
+Write-Verbose "Enabling Remote Desktop Rules"
+foreach ($rule in Get-NetFirewallRule -DisplayGroup "Remote Desktop") {
+    Set-NetFirewallRule -Name $rule.Name -Enabled True
+}
