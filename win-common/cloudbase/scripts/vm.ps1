e4b1b3b7eca70570cf5d06953500cc15956c55bb
Will Rouesnel <wrouesnel@wrouesnel.com>
2021-04-23 12:31:10 +1000

    Win 10: give up and just remove edge

--- win-common/cloudbase/scripts/vm.ps1
+++ win-common/cloudbase/scripts/vm.ps1
@@ -16,5 +16,19 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer
     -Value "Off" `
     -Force
 
+# Set layout to something sensible
+Copy-Item "C:\Drivers\scripts\StartLayout.xml" `
+    -Destination "$env:USERPROFILE\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"
+
 Copy-Item "C:\Drivers\scripts\StartLayout.xml" `
     -Destination "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml"
+
+# Remove OneDrive
+pushd "$env:SystemRoot\SysWOW64"
+.\OneDriveSetup.exe /uninstall
+popd
+
+# Remove Edge
+pushd "`"${env:ProgramFiles(x86)}\Microsoft\Edge\Application\*\Installer`""
+.\setup.exe -uninstall -system-level -verbose-logging -force-uninstall
+popd
